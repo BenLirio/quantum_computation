317a29cf5690c7209f28b4f21eb680394907c452
Ben Lirio <benjlirio@gmail.com>
2022-01-13 20:34:54 +0000

    Add qbit in python

---
+++ python/qbit.py
@@ -0,0 +1,18 @@
+import math
+
+PRECISION = 1e-5
+
+class Qbit:
+    def __init__(self):
+        self.vals = [complex(math.sqrt(2)/2,0), complex(math.sqrt(2)/2,0)]
+    def is_valid(self):
+        if sum([abs(v)**2 for v in self.vals]) - 1.0 < PRECISION:
+            return True
+        else:
+            print(sum([abs(v)**2 for v in self.vals]))
+            return False
+    def __str__(self):
+        return '['+', '.join([f"({round(v.real,2)}, {round(v.imag,2)}j)" for v in self.vals])+']'
+
+print(Qbit())
+print(Qbit().is_valid())
