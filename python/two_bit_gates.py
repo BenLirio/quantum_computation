15c1fecf82dc60b359ed0ea5289f95088b907ab6
Ben Lirio <benjlirio@gmail.com>
2022-01-14 02:47:53 +0000

    Create all permutations of 2bit gates

---
+++ python/two_bit_gates.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+c = complex
+gates = np.array([
+    [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0]
+    ],
+    [
+        [0, 1, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ],
+    [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ],
+    [
+        [0, 0, 0, 1],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0],
+        [1, 0, 0, 0]
+    ],
+])
+
+x = np.array([
+    [1],
+    [0],
+    [1],
+    [0]
+])
+
+t = np.transpose
+mul = np.matmul
+eye = np.eye(4, dtype=int)
+
+i = 0
+for A in gates:
+    i += 1
+    print(f"Gate {i}:")
+    print(mul(A, x))
+    if (mul(t(A), A) != eye).any():
+        print(A)
+    print()
